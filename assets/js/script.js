916a5e46245013cea8abb1bfe3ccb27f693cc011
Husnain-WPBrigade <102015744+Husnain-WPBrigade@users.noreply.github.com>
2022-05-21 15:44:17 +0500

    Some small bug fixed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,45 @@
 const menuBtn = document.querySelector(".utility__menu");
 const navbar = document.querySelector(".nav");
-const menuClosebtn = document.querySelector('.menu-close')
+const menuClosebtn = document.querySelector(".menu-close");
 const notificationBtn = document.querySelector(".close--container");
 const notificationBar = document.querySelector(".notification");
 const videoPlayBtn = document.querySelectorAll(".play-video");
 const videoCloseBtn = document.querySelector(".video__close-btn");
 const videoBox = document.querySelector(".video--box");
 const navLinks = document.querySelectorAll(".nav__link-item");
-const inputSearch = document.querySelector('#input--search')
-const inputSearchIcon = document.querySelector('.input__search')
-const inputCrossIcon = document.querySelector('.input-cross')
-const input = document.querySelector('.nav--input')
+
+const navInput = document.querySelector(".nav--input");
+const inputClearBtn = document.querySelector(".input-cross");
+const inputSearchBtn = document.querySelector(".input-search");
+
+navInput.addEventListener("click", searchInputActive);
+inputClearBtn.addEventListener("click", searchInputEmpty);
+
+function searchInputActive() {
+  inputSearchBtn.classList.add("input-hide");
+  inputClearBtn.classList.remove("input-hide");
+}
+
+function searchInputEmpty(e) {
+  e.preventDefault(e);
+  navInput.value = "";
+  inputSearchBtn.classList.remove("input-hide");
+  inputClearBtn.classList.add("input-hide");
+}
 
 // menuBtn.addEventListener('click', )
 
 menuBtn.addEventListener("click", () => {
-  menuClosebtn.classList.remove('menu-btn-hide')
-  menuBtn.classList.add('menu-btn-hide')
+  menuClosebtn.classList.remove("menu-btn-hide");
+  menuBtn.classList.add("menu-btn-hide");
   navbar.classList.toggle("active");
 });
 
-menuClosebtn.addEventListener('click', () => {
-  navbar.classList.toggle('active')
-  menuClosebtn.classList.add('menu-btn-hide')
-  menuBtn.classList.remove('menu-btn-hide')
-})
+menuClosebtn.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  menuClosebtn.classList.add("menu-btn-hide");
+  menuBtn.classList.remove("menu-btn-hide");
+});
 
 notificationBtn.addEventListener("click", () => {
   notificationBar.classList.add("close");
@@ -39,19 +54,6 @@ videoPlayBtn.forEach((playBtn) => {
 videoCloseBtn.addEventListener("click", () => {
   videoBox.classList.remove("video-show");
 });
-
-inputSearch.addEventListener('keypress', ()=> {
-  inputSearchIcon.classList.add('input-hide')
-  inputCrossIcon.classList.remove('input-hide')
-})
-
-
-inputCrossIcon.addEventListener('click', () => {
-  input.value = ''
-  inputSearchIcon.classList.remove('input-hide')
-  inputCrossIcon.classList.add('input-hide')
-})
-
 
 // Hero Slider
 var swiper1 = new Swiper(".hero__slider", {
@@ -96,4 +98,3 @@ var swiper2 = new Swiper(".card__slider", {
     prevEl: ".card__prev",
   },
 });
-
